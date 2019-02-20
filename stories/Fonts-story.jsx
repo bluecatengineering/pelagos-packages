b9b5611b0eefb029d39a833cc097cb3ee41f05b1
JC Estibariz <jestibariz@bluecatnetworks.com>
2019-02-20 14:03:05 -0500

    Replace font definitions

--- stories/Fonts-story.jsx
+++ stories/Fonts-story.jsx
@@ -3,43 +3,44 @@ import {storiesOf} from '@storybook/react';
 
 import './Fonts-story.less';
 
-const fonts = [
-	['font-indicator', 10, 400],
-	['font-notice', 10, 400],
-	['font-footer', 10, 400],
-	['font-label', 10, 600],
-	['font-error', 10, 600],
-	['font-field-hint', 12, 400],
-	['font-crumbs', 12, 400],
-	['font-body-small', 12, 400],
-	['font-cmd-header', 12, 600],
-	['font-table-title', 12, 600],
-	['font-button-small', 12, 600],
-	['font-body-disabled', 14, 400],
-	['font-body-dim', 14, 400],
-	['font-body-medium', 14, 400],
-	['font-block-action', 14, 400],
-	['font-important', 14, 600],
-	['font-button-medium', 14, 600],
-	['font-field-hint-large', 16, 400],
-	['font-empty-msg', 16, 400],
-	['font-body-large', 16, 400],
-	['font-product', 16, 400],
-	['font-title-small', 16, 600],
-	['font-button-large', 16, 600],
-	['font-title-medium', 18, 400],
-	['font-title-large', 24, 400],
-	['font-display-large', 68, 300],
+const fontColors = ['color1', 'color2', 'color3', 'red', 'burnt', 'bondi', 'sky'];
+
+const fontTable = [
+	{name: '10-400', colors: {color1: 'footer', color3: 'indicator', burnt: 'notice'}},
+	{name: '10-600-uc', colors: {color1: 'error', color2: 'label'}},
+	{name: '12-400', colors: {color1: 'body-small', color2: 'crumbs', sky: 'field-hint'}},
+	{name: '12-600-uc', colors: {color1: 'button-small', color2: 'table-title', color3: 'cmd-header'}},
+	{name: '14-400', colors: {color1: 'body-medium', color2: 'body-dim', color3: 'body-disabled', red: 'block-action'}},
+	{name: '14-600', colors: {color1: 'important'}},
+	{name: '14-600-uc', colors: {color1: 'button-medium'}},
+	{name: '16-400', colors: {color1: 'body-large', color3: 'empty-msg'}},
+	{name: '16-400-sp2', colors: {bondi: 'product'}},
+	{name: '16-600', colors: {color1: 'title-small'}},
+	{name: '16-600-uc', colors: {color1: 'button-large'}},
+	{name: '18-400', colors: {color1: 'title-medium'}},
+	{name: '24-400', colors: {color1: 'title-large'}},
+	{name: '68-300', colors: {color1: 'display-large'}},
 ];
 
 storiesOf('Fonts', module).add('all fonts', () => (
 	<table className="FontsStory">
+		<thead>
+			<tr>
+				<th>Properties</th>
+				{fontColors.map(color => (
+					<th key={color}>{color}</th>
+				))}
+			</tr>
+		</thead>
 		<tbody>
-			{fonts.map(([name, size, weight]) => (
+			{fontTable.map(({name, colors}) => (
 				<tr key={name}>
-					<td>{size}</td>
-					<td>{weight}</td>
-					<td className={name}>{name.substr(5)}</td>
+					<td>{name}</td>
+					{fontColors.map(color => (
+						<td key={color} className={'font-' + colors[color]}>
+							{colors[color]}
+						</td>
+					))}
 				</tr>
 			))}
 		</tbody>
