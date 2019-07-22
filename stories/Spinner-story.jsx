3c5aa6ccb483b4b3c2c8722c825bb21ded90a317
JC Estibariz <jestibariz@bluecatnetworks.com>
2019-07-22 16:56:03 -0400

    Enable stylelint

--- stories/Spinner-story.jsx
+++ stories/Spinner-story.jsx
@@ -5,10 +5,10 @@ import {Spinner} from '../src';
 
 storiesOf('Spinner', module).add('all states', () => (
 	<div className="Story__row">
-		<div className="Story__group" style={{flex: '1'}}>
+		<div className="Story__group" style={{flex: '1', position: 'relative'}}>
 			<Spinner />
 		</div>
-		<div className="Story__group" style={{flex: '1'}}>
+		<div className="Story__group" style={{flex: '1', position: 'relative'}}>
 			<Spinner size="large" />
 		</div>
 	</div>
