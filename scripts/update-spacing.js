d8ad3737106b153a271c52e8ce83d8a501caf7da
Sam Wu <swu@bluecatnetworks.com>
2019-09-09 14:02:53 -0400

    Define spacing using a yaml file

---
+++ scripts/update-spacing.js
@@ -0,0 +1,16 @@
+const {readFileSync, writeFileSync} = require('fs');
+
+const {safeLoad} = require('js-yaml');
+
+const IN = 'defs/spacing.yaml';
+const LESS = 'less/spacing.less';
+
+const HEADER = [
+	'// This file was generated from defs/spacing.yaml',
+	'// it will be re-generated on commit, to generate manually run `npm run update-spacing`',
+	'',
+];
+
+const spacing = Object.entries(safeLoad(readFileSync(IN, 'utf8')));
+
+writeFileSync(LESS, HEADER.concat(spacing.map(([key, {size}]) => '@' + key + ': ' + size + 'px;'), '').join('\n'));
