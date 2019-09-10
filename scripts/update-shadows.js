c5a28ec751445a30b6492508a45cb6dfd2688928
Sam Wu <swu@bluecatnetworks.com>
2019-09-10 14:14:52 -0400

    Define shadows using a yaml file

---
+++ scripts/update-shadows.js
@@ -0,0 +1,16 @@
+const {readFileSync, writeFileSync} = require('fs');
+
+const {safeLoad} = require('js-yaml');
+
+const IN = 'defs/shadows.yaml';
+const LESS = 'less/shadows.less';
+
+const HEADER = [
+	'// This file was generated from defs/shadows.yaml',
+	'// it will be re-generated on commit, to generate manually run `npm run update-shadows`',
+	'',
+];
+
+const spacing = Object.entries(safeLoad(readFileSync(IN, 'utf8')));
+
+writeFileSync(LESS, HEADER.concat(spacing.map(([key, {value}]) => '@' + key + ': ' + value + ';'), '').join('\n'));
