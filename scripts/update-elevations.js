cdf85c837855835f43d139499aad409fa4dc12c0
JC Estibariz <jestibariz@bluecatnetworks.com>
2019-09-11 17:43:05 -0400

    Implement guidelines for dark mode

---
+++ scripts/update-elevations.js
@@ -0,0 +1,31 @@
+const {readFileSync, writeFileSync} = require('fs');
+
+const {safeLoad} = require('js-yaml');
+
+const IN = 'defs/elevations.yaml';
+const LESS = 'less/elevations.less';
+
+const HEADER = [
+	'// This file was generated from defs/elevations.yaml',
+	'// it will be re-generated on commit, to generate manually run `npm run update-elevations`',
+	'',
+	"@import 'colors';",
+	"@import 'shadows';",
+	'',
+];
+
+const elevations = Object.entries(safeLoad(readFileSync(IN, 'utf8')));
+
+writeFileSync(
+	LESS,
+	HEADER.concat(
+		elevations.map(
+			([key, {bg, shadow}]) =>
+				`@${key}: {
+	background-color: @${bg};
+	box-shadow: @${shadow};
+};`
+		),
+		''
+	).join('\n')
+);
