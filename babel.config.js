e982a893d567b101f41cf77e3211219915f57c2d
JC Estibariz <jestibariz@bluecatnetworks.com>
2019-10-07 16:17:57 -0400

    Remove duplicated react preset

--- babel.config.js
+++ babel.config.js
@@ -4,15 +4,13 @@ module.exports = {
 	env: {
 		test: {
 			sourceMaps: 'both',
-			presets: [['@babel/env', {loose: true, targets: {node: true}}], ['@babel/react', {useBuiltIns: true}]],
+			presets: [['@babel/env', {loose: true, targets: {node: true}}]],
 		},
 		es5: {
 			presets: [['@babel/env', {loose: true, modules: false, useBuiltIns: 'usage', corejs: 3, targets: {ie: '11'}}]],
 		},
 		es6: {
-			presets: [
-				['@babel/env', {loose: true, modules: false, useBuiltIns: false, targets: {chrome: '69', firefox: '61'}}],
-			],
+			presets: [['@babel/env', {loose: true, modules: false, targets: {chrome: '69', firefox: '61'}}]],
 		},
 	},
 };
