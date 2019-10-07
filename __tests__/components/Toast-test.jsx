d10a166ba5691164e27c9828cad5bc7f6eef4b62
JC Estibariz <jestibariz@bluecatnetworks.com>
2019-10-07 16:00:06 -0400

    Include modal backdrop for fatal errors in Toast

--- __tests__/components/Toast-test.jsx
+++ __tests__/components/Toast-test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import Toast from '../../src/components/Toast';
+import {hasFatalError} from '../../src/toasts/ToastFunctions';
 
 jest.unmock('../../src/components/Toast');
 
@@ -11,6 +12,14 @@ describe('Toast', () => {
 			const messages = [{type: 'a', text: 'Test 1'}, {type: 'b', text: 'Test 2'}];
 			const wrapper = shallow(<Toast messages={messages} />);
 			expect(wrapper.getElement()).toMatchSnapshot();
+		});
+
+		it('renders modal backdrop when hasFatalError returns true', () => {
+			const messages = [{}];
+			hasFatalError.mockReturnValue(true);
+			const wrapper = shallow(<Toast messages={messages} />);
+			expect(wrapper.getElement()).toMatchSnapshot();
+			expect(hasFatalError.mock.calls).toEqual([[messages]]);
 		});
 	});
 });
--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -3,23 +3,29 @@ import PropTypes from 'prop-types';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
 import CSSTransition from 'react-transition-group/CSSTransition';
 
+import {hasFatalError} from '../toasts/ToastFunctions';
+
 import ToastMessage from './ToastMessage';
 import './Toast.less';
 
+/** Displays messages as toasts. */
 const Toast = ({messages, onRemove}) => (
 	<div className="Toast">
-		<TransitionGroup>
+		<TransitionGroup className="Toast__messages">
 			{messages.map((m, i) => (
 				<CSSTransition key={i} classNames="swing" timeout={{enter: 500, exit: 300}}>
 					<ToastMessage message={m} onRemove={onRemove} />
 				</CSSTransition>
 			))}
 		</TransitionGroup>
+		{hasFatalError(messages) && <div className="Toast__backdrop" />}
 	</div>
 );
 
 Toast.propTypes = {
+	/** Messages to display. */
 	messages: PropTypes.array,
+	/** Function to remove a message. */
 	onRemove: PropTypes.func,
 };
 
