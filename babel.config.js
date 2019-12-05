b40ce7263ab7034f935f6d192a42a857c335e803
JC Estibariz <jestibariz@bluecatnetworks.com>
2019-12-05 14:08:07 -0500

    Transform ICU keys in JS files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+const hashLength = 5;
+
 module.exports = {
 	presets: [['@babel/react', {useBuiltIns: true}]],
 	plugins: ['@babel/plugin-proposal-class-properties'],
@@ -8,9 +10,11 @@ module.exports = {
 		},
 		es5: {
 			presets: [['@babel/env', {loose: true, modules: false, useBuiltIns: 'usage', corejs: 3, targets: {ie: '11'}}]],
+			plugins: [['@bluecat/transform-hash-icu-key', {hashLength}]],
 		},
 		es6: {
 			presets: [['@babel/env', {loose: true, modules: false, targets: {chrome: '69', firefox: '61'}}]],
+			plugins: [['@bluecat/transform-hash-icu-key', {hashLength}]],
 		},
 	},
 };
