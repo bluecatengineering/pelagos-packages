4b8807f1072fac3c5204cff7b1fdf818d127f8bb
JC Estibariz <jestibariz@bluecatnetworks.com>
2019-12-05 14:28:52 -0500

    Add key handling to Hamburger

--- __tests__/components/Hamburger-test.js
+++ __tests__/components/Hamburger-test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import Hamburger from '../../src/components/Hamburger';
+import useButtonKeyHandler from '../../src/hooks/useButtonKeyHandler';
 
 jest.unmock('../../src/components/Hamburger');
 
@@ -24,6 +25,15 @@ describe('Hamburger', () => {
 			const wrapper = shallow(<Hamburger onClick={onClick} />);
 			wrapper.find('[role="button"]').simulate('click');
 			expect(onClick).toHaveBeenCalledTimes(1);
+		});
+
+		it('calls the key handler when a key is pressed', () => {
+			const onClick = jest.fn();
+			const event = {keyCode: 13};
+			useButtonKeyHandler.mockImplementation(v => v);
+			const wrapper = shallow(<Hamburger onClick={onClick} />);
+			wrapper.find('[role="button"]').simulate('keydown', event);
+			expect(onClick.mock.calls).toEqual([[event]]);
 		});
 	});
 });
--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import useButtonKeyHandler from '../hooks/useButtonKeyHandler';
+
 import './Hamburger.less';
 
 const Hamburger = ({active, onClick, ...props}) => (
-	<div {...props} className="Hamburger" role="button" tabIndex="0" onClick={onClick}>
+	<div
+		{...props}
+		className="Hamburger"
+		role="button"
+		tabIndex="0"
+		onClick={onClick}
+		onKeyDown={useButtonKeyHandler(onClick)}>
 		<div className={'Hamburger__inner' + (active ? ' Hamburger--active' : '')} />
 	</div>
 );
