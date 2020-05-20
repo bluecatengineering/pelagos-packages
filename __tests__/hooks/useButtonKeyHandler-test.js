082fa5646ed934f4455f6fe2745f8099332ab784
JC Estibariz <jestibariz@bluecatnetworks.com>
2020-05-20 12:19:59 -0400

    Call click method on the target
    Avoids passing the click handler

--- __tests__/hooks/useButtonKeyHandler-test.js
+++ __tests__/hooks/useButtonKeyHandler-test.js
@@ -4,39 +4,47 @@ jest.unmock('../../src/hooks/useButtonKeyHandler');
 
 describe('useButtonKeyHandler', () => {
 	it('calls onClick when enter is pressed', () => {
-		const onClick = jest.fn();
-		const event = {keyCode: 13, preventDefault: jest.fn()};
-		useButtonKeyHandler(onClick)(event);
-		expect(onClick.mock.calls).toEqual([[event]]);
+		const click = jest.fn();
+		const event = {
+			keyCode: 13,
+			target: {click},
+			preventDefault: jest.fn(),
+			nativeEvent: {stopImmediatePropagation: jest.fn()},
+		};
+		useButtonKeyHandler()(event);
+		expect(click.mock.calls).toEqual([[]]);
 		expect(event.preventDefault).toHaveBeenCalled();
+		expect(event.nativeEvent.stopImmediatePropagation).toHaveBeenCalled();
 	});
 
 	it('calls onClick when space is pressed', () => {
-		const onClick = jest.fn();
-		const event = {keyCode: 32, preventDefault: jest.fn()};
-		useButtonKeyHandler(onClick)(event);
-		expect(onClick.mock.calls).toEqual([[event]]);
+		const click = jest.fn();
+		const event = {
+			keyCode: 32,
+			target: {click},
+			preventDefault: jest.fn(),
+			nativeEvent: {stopImmediatePropagation: jest.fn()},
+		};
+		useButtonKeyHandler()(event);
+		expect(click.mock.calls).toEqual([[]]);
 		expect(event.preventDefault).toHaveBeenCalled();
+		expect(event.nativeEvent.stopImmediatePropagation).toHaveBeenCalled();
 	});
 
 	it('does not call onClick when any other key is pressed', () => {
-		const onClick = jest.fn();
 		const event = {keyCode: 65, preventDefault: jest.fn()};
-		useButtonKeyHandler(onClick)(event);
-		expect(onClick).not.toHaveBeenCalled();
+		useButtonKeyHandler()(event);
 		expect(event.preventDefault).not.toHaveBeenCalled();
 	});
 
 	it('does not call onClick when any modifier is set', () => {
-		const onClick = jest.fn();
 		const preventDefault = jest.fn();
 
-		useButtonKeyHandler(onClick)({keyCode: 13, shiftKey: true, preventDefault});
-		useButtonKeyHandler(onClick)({keyCode: 13, ctrlKey: true, preventDefault});
-		useButtonKeyHandler(onClick)({keyCode: 13, altKey: true, preventDefault});
-		useButtonKeyHandler(onClick)({keyCode: 13, metaKey: true, preventDefault});
+		useButtonKeyHandler()({keyCode: 13, shiftKey: true, preventDefault});
+		useButtonKeyHandler()({keyCode: 13, ctrlKey: true, preventDefault});
+		useButtonKeyHandler()({keyCode: 13, altKey: true, preventDefault});
+		useButtonKeyHandler()({keyCode: 13, metaKey: true, preventDefault});
 
-		expect(onClick).not.toHaveBeenCalled();
 		expect(preventDefault).not.toHaveBeenCalled();
 	});
 });
--- src/hooks/useButtonKeyHandler.js
+++ src/hooks/useButtonKeyHandler.js
@@ -1,6 +1,6 @@
 import {useCallback} from 'react';
 
-export default (onClick) =>
+export default () =>
 	useCallback(
 		(event) =>
 			!event.shiftKey &&
@@ -8,7 +8,7 @@ export default (onClick) =>
 			!event.altKey &&
 			!event.metaKey &&
 			(event.keyCode === 13 || event.keyCode === 32)
-				? (event.preventDefault(), onClick(event))
+				? (event.preventDefault(), event.nativeEvent.stopImmediatePropagation(), event.target.click())
 				: null,
-		[onClick]
+		[]
 	);
