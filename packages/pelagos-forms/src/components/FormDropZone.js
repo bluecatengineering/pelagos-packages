437485407377e350c2925f1fd97583d2f585e4d2
JC Estibariz <jestibariz@bluecatnetworks.com>
2022-10-25 11:44:09 -0400

    feat: deprecate DropZone component

--- packages/pelagos-forms/src/components/FormDropZone.js
+++ packages/pelagos-forms/src/components/FormDropZone.js
@@ -8,4 +8,5 @@ export const mapState = ({extra, error, setValue, setExtra}) => ({
 		args.length === 1 ? (setExtra(args[0].name), setValue(args[0])) : (setExtra(args[0]), setValue(args[1])),
 });
 
+/** @deprecated use FormFileUploader instead. */
 export default connect(DropZone, mapState);
