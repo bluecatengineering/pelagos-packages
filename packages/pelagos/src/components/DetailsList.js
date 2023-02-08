9e985326b03bd24abd1da4171263b4f021614747
JC Estibariz <jestibariz@bluecatnetworks.com>
2023-02-08 10:42:11 -0500

    feat: add info text properties to DetailsList

--- packages/pelagos/src/components/DetailsList.js
+++ packages/pelagos/src/components/DetailsList.js
@@ -6,11 +6,16 @@ import DetailRegion from './DetailRegion';
 import './DetailsList.less';
 
 /** A list for the details panel. */
-const DetailsList = ({id, className, label, list, renderItem}) => {
+const DetailsList = ({id, className, label, infoText, infoTextPlacement, list, renderItem}) => {
 	id = useRandomId(id);
 	const labelId = `${id}-label`;
 	return (
-		<DetailRegion id={labelId} className={className} label={label}>
+		<DetailRegion
+			id={labelId}
+			className={className}
+			label={label}
+			infoText={infoText}
+			infoTextPlacement={infoTextPlacement}>
 			<ul id={id} className="DetailsList__grid" aria-labelledby={labelId}>
 				{list.map((item, index) => (
 					<li key={index}>{renderItem(item, 'DetailsList__item', index)}</li>
@@ -27,6 +32,10 @@ DetailsList.propTypes = {
 	className: PropTypes.string,
 	/** The label text. */
 	label: PropTypes.string.isRequired,
+	/** The text of the info tooltip to display. */
+	infoText: PropTypes.string,
+	/** The placement of the info tooltip relative to the icon. */
+	infoTextPlacement: PropTypes.oneOf(['left', 'right', 'top', 'bottom']),
 	/** The list to display. */
 	list: PropTypes.array.isRequired,
 	/** Function to render each list item. */
