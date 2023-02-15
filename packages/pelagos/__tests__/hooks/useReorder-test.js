3c4b202422dc329d27508dca1fd06b2d1c2164cd
JC Estibariz <jestibariz@bluecatnetworks.com>
2023-02-15 18:10:14 -0500

    fix!: use a local live element in useReorder
    BREAKING-CHANGE: Users must accept second ref and add a live element close to the list.

--- packages/pelagos/__tests__/hooks/useReorder-test.js
+++ packages/pelagos/__tests__/hooks/useReorder-test.js
@@ -2,7 +2,6 @@ import {useEffect, useRef} from 'react';
 import reorder from '@bluecateng/smooth-reorder';
 
 import useReorder from '../../src/hooks/useReorder';
-import setLiveText from '../../src/functions/setLiveText';
 
 jest.unmock('../../src/hooks/useReorder');
 
@@ -13,16 +12,22 @@ describe('useReorder', () => {
 		const selector = '.Test';
 		const handleSelector = '.Test__handle';
 		const container = {};
-		const ref = {current: container};
+		const containerRef = {current: container};
+		const live = {};
+		const liveRef = {current: live};
+		const getAttribute = jest.fn().mockReturnValue('attribute-value');
 		const setAttribute = jest.fn();
 		const removeAttribute = jest.fn();
 		const list = ['foo', 'bar'];
 		const getElementName = (element) => list[element.dataset.index];
 		const updateList = jest.fn();
 
-		useRef.mockReturnValueOnce(ref);
+		useRef.mockReturnValueOnce(containerRef).mockReturnValueOnce(liveRef);
 
-		expect(useReorder(selector, handleSelector, list.length, getElementName, updateList)).toBe(ref);
+		expect(useReorder(selector, handleSelector, list.length, getElementName, updateList)).toEqual([
+			containerRef,
+			liveRef,
+		]);
 		expect(useEffect.mock.calls[0]).toEqual([
 			expect.any(Function),
 			[selector, handleSelector, getElementName, updateList, list.length],
@@ -44,31 +49,48 @@ describe('useReorder', () => {
 		]);
 
 		const {onStart, onMove, onFinish, onCancel} = reorder.mock.calls[0][1];
-		const element1 = {dataset: {index: '0'}, previousSibling: {dataset: {index: '1'}}, setAttribute, removeAttribute};
-		const element2 = {dataset: {index: '1'}, removeAttribute};
-		const element3 = {dataset: {index: '0'}, removeAttribute};
+		const element1 = {
+			dataset: {index: '0'},
+			previousSibling: {dataset: {index: '1'}},
+			getAttribute,
+			setAttribute,
+			removeAttribute,
+		};
+		const element2 = {dataset: {index: '1'}, setAttribute, removeAttribute};
+		const element3 = {dataset: {index: '0'}, setAttribute, removeAttribute};
 		onStart(element1, 0);
+		expect(live.textContent).toBe(
+			'foo grabbed. Current position in list: 1 of 2. Press up or down arrow keys to change position, space bar to drop, Escape key to cancel.'
+		);
 		onMove(element1, 1);
+		jest.runOnlyPendingTimers();
+		expect(live.textContent).toBe('foo. Current position in list: 2 of 2.');
 		onFinish(element1);
+		expect(live.textContent).toBe('foo dropped. Final position in list: 2 of 2.');
 		onFinish(element2);
+		expect(live.textContent).toBe('bar dropped. Final position in list: 1 of 2.');
 		onFinish(element3);
+		expect(live.textContent).toBe('foo dropped. Final position in list: 1 of 2.');
 		onCancel(element1);
-		expect(setLiveText.mock.calls).toEqual([
-			[
-				'foo grabbed. Current position in list: 1 of 2. Press up or down arrow keys to change position, space bar to drop, Escape key to cancel.',
-			],
-			['foo. Current position in list: 2 of 2.'],
-			['foo dropped. Final position in list: 2 of 2.'],
-			['bar dropped. Final position in list: 1 of 2.'],
-			['foo dropped. Final position in list: 1 of 2.'],
-			['foo reorder cancelled.'],
-		]);
+		expect(live.textContent).toBe('foo reorder cancelled.');
 		expect(updateList.mock.calls).toEqual([
 			[0, 1],
 			[1, 0],
 		]);
-		expect(setAttribute.mock.calls).toEqual([['aria-hidden', 'true']]);
+		expect(setAttribute.mock.calls).toEqual([
+			['aria-hidden', 'true'],
+			['aria-describedby', 'attribute-value'],
+			['aria-describedby', 'attribute-value'],
+			['aria-describedby', 'attribute-value'],
+			['aria-describedby', 'attribute-value'],
+		]);
 		jest.runOnlyPendingTimers();
-		expect(removeAttribute.mock.calls).toEqual([['aria-hidden'], ['aria-hidden'], ['aria-hidden'], ['aria-hidden']]);
+		expect(removeAttribute.mock.calls).toEqual([
+			['aria-describedby'],
+			['aria-hidden'],
+			['aria-hidden'],
+			['aria-hidden'],
+			['aria-hidden'],
+		]);
 	});
 });
