3eb68d1789960d0200cb39b070917724a61d0443
JC Estibariz <jestibariz@bluecatnetworks.com>
2023-02-15 18:10:14 -0500

    fix!: use a local live element in useReorder
    BREAKING-CHANGE: Users must accept second ref and add a live element close to the list.

--- packages/pelagos/src/hooks/useReorder.js
+++ packages/pelagos/src/hooks/useReorder.js
@@ -2,18 +2,18 @@ import {useEffect, useRef} from 'react';
 import reorder from '@bluecateng/smooth-reorder';
 import {t} from '@bluecateng/l10n.macro';
 
-import setLiveText from '../functions/setLiveText';
-
 const removeAriaHidden = (element) => setTimeout(() => element.removeAttribute('aria-hidden'), 500);
 
+const setLiveText = (ref, text) => (ref.current.textContent = text);
+
 /**
- * Returns a React ref which enables reordering child nodes using drag and drop.
+ * Returns two React refs which enable reordering child nodes using drag and drop.
  * @param {string} selector CSS selector for elements to reorder.
  * @param {string} handleSelector CSS selector for reorder handle.
  * @param {number} count the number of elements.
  * @param {function(Element): string} getElementName returns the name of the specified element.
  * @param {function(number, number): void} onChange invoked when the list is changed, the first argument is the original index of the element moved, the second is the target index.
- * @return {MutableRefObject<Element>} React ref.
+ * @return {Array<MutableRefObject<Element>>} reorderRef and liveRef.
  *
  * @example
  * import {useCallback} from 'react';
@@ -26,45 +26,63 @@ const removeAriaHidden = (element) => setTimeout(() => element.removeAttribute('
  *   const onChange = useCallback((fromIndex, toIndex) => {
  *     // move item
  *   }, []);
- *   const reorder = useReorder('.Child', '.Handle', list.length, getElementName, onChange);
- *   return <ol ref={reorder}>...</ol>;
+ *   const [reorderRef, liveRef] = useReorder('.Child', '.Handle', list.length, getElementName, onChange);
+ *   return (
+ *     <div>
+ *       <div className="assistive-text" aria-live="polite" ref={liveRef} />
+ *       <ol ref={reorderRef}>...</ol>
+ *     </div>
+ *   );
  * };
  */
 const useReorder = (selector, handleSelector, count, getElementName, onChange) => {
 	const reorderRef = useRef(null);
-	useEffect(
-		() =>
-			reorder(reorderRef.current, {
-				selector,
-				focusSelector: selector,
-				handleSelector,
-				onStart: (element, position) => (
-					element.setAttribute('aria-hidden', 'true'),
-					setLiveText(
-						t`${getElementName(element)} grabbed. Current position in list: ${
-							position + 1
-						} of ${count}. Press up or down arrow keys to change position, space bar to drop, Escape key to cancel.`
-					)
+	const liveRef = useRef(null);
+	useEffect(() => {
+		let describeId;
+		return reorder(reorderRef.current, {
+			selector,
+			focusSelector: selector,
+			handleSelector,
+			onStart: (element, position) => {
+				describeId = element.getAttribute('aria-describedby');
+				element.removeAttribute('aria-describedby');
+				element.setAttribute('aria-hidden', 'true');
+				setLiveText(
+					liveRef,
+					t`${getElementName(element)} grabbed. Current position in list: ${
+						position + 1
+					} of ${count}. Press up or down arrow keys to change position, space bar to drop, Escape key to cancel.`
+				);
+			},
+			onMove: (element, position) =>
+				setTimeout(
+					() =>
+						setLiveText(liveRef, t`${getElementName(element)}. Current position in list: ${position + 1} of ${count}.`),
+					150
 				),
-				onMove: (element, position) =>
-					setLiveText(t`${getElementName(element)}. Current position in list: ${position + 1} of ${count}.`),
-				onFinish: (element) => {
-					const fromIndex = +element.dataset.index;
-					const tmp = element.previousSibling ? +element.previousSibling.dataset.index + 1 : 0;
-					const toIndex = fromIndex < tmp ? tmp - 1 : tmp;
-					if (fromIndex !== tmp) {
-						onChange(fromIndex, toIndex);
-					}
-					setLiveText(t`${getElementName(element)} dropped. Final position in list: ${toIndex + 1} of ${count}.`);
-					removeAriaHidden(element);
-				},
-				onCancel: (element) => (
-					setLiveText(t`${getElementName(element)} reorder cancelled.`), removeAriaHidden(element)
-				),
-			}),
-		[selector, handleSelector, getElementName, onChange, count]
-	);
-	return reorderRef;
+			onFinish: (element) => {
+				const fromIndex = +element.dataset.index;
+				const tmp = element.previousSibling ? +element.previousSibling.dataset.index + 1 : 0;
+				const toIndex = fromIndex < tmp ? tmp - 1 : tmp;
+				if (fromIndex !== tmp) {
+					onChange(fromIndex, toIndex);
+				}
+				setLiveText(
+					liveRef,
+					t`${getElementName(element)} dropped. Final position in list: ${toIndex + 1} of ${count}.`
+				);
+				element.setAttribute('aria-describedby', describeId);
+				removeAriaHidden(element);
+			},
+			onCancel: (element) => {
+				setLiveText(liveRef, t`${getElementName(element)} reorder cancelled.`);
+				element.setAttribute('aria-describedby', describeId);
+				removeAriaHidden(element);
+			},
+		});
+	}, [selector, handleSelector, getElementName, onChange, count]);
+	return [reorderRef, liveRef];
 };
 
 export default useReorder;
