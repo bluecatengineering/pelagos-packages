22a9d1d3d99473d664a5bb58bc60af28d0680f77
JC Estibariz <jestibariz@bluecatnetworks.com>
2023-04-27 18:19:36 -0400

    fix: allow selection from popup list while focus trap is active

--- packages/pelagos/__tests__/hooks/useFocusTrap-test.js
+++ packages/pelagos/__tests__/hooks/useFocusTrap-test.js
@@ -11,14 +11,19 @@ describe('useFocusTrap', () => {
 		const ref = {current};
 		const initialFocus = '#test';
 		const onClose = jest.fn();
+		const closest = jest.fn().mockReturnValue({});
 		const trap = {activate: jest.fn(), deactivate: jest.fn()};
 		createFocusTrap.mockReturnValue(trap);
 		useFocusTrap(ref, initialFocus, onClose);
 		expect(useEffect.mock.calls).toEqual([[expect.any(Function), [ref, initialFocus, onClose]]]);
 		expect(useEffect.mock.calls[0][0]()).toBe(trap.deactivate);
 		expect(createFocusTrap.mock.calls).toEqual([
-			[current, {initialFocus, clickOutsideDeactivates: true, onDeactivate: onClose}],
+			[current, {initialFocus, clickOutsideDeactivates: expect.any(Function), onDeactivate: onClose}],
 		]);
 		expect(trap.activate.mock.calls).toEqual([[]]);
+
+		const {clickOutsideDeactivates} = createFocusTrap.mock.calls[0][1];
+		expect(clickOutsideDeactivates({target: {closest}})).toBe(false);
+		expect(closest.mock.calls).toEqual([['[role="listbox"]']]);
 	});
 });
