291d23c39c2200598f0ec090f032a4b5b683fae4
JC Estibariz <jestibariz@bluecatnetworks.com>
2023-05-16 13:26:53 -0400

    fix: use specified colours in SideNav

--- packages/pelagos/src/sideNav/SideNav.js
+++ packages/pelagos/src/sideNav/SideNav.js
@@ -1,16 +1,14 @@
 import {forwardRef} from 'react';
 import PropTypes from 'prop-types';
 
-import Layer from '../components/Layer';
-
 import passSideNavActive from './passSideNavActive';
 import './SideNav.less';
 
 /** Side navigation component. */
 const SideNav = forwardRef(({className, active, children, ...props}, ref) => (
-	<Layer {...props} as="nav" className={`SideNav${className ? ` ${className}` : ''}`} aria-hidden={!active} ref={ref}>
+	<nav {...props} className={`SideNav${className ? ` ${className}` : ''}`} aria-hidden={!active} ref={ref}>
 		{passSideNavActive(active, children)}
-	</Layer>
+	</nav>
 ));
 
 SideNav.displayName = 'SideNav';
