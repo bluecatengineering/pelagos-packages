f1d663ea669d3f215b1890cf86e8dae09aef7fdd
JC Estibariz <jestibariz@bluecatnetworks.com>
2023-07-13 16:15:33 -0400

    docs: add "Try it out" stories for a few components

--- packages/pelagos/src/components/ConfirmDialog.stories.js
+++ packages/pelagos/src/components/ConfirmDialog.stories.js
@@ -1,10 +1,28 @@
+import {useCallback, useState} from 'react';
+import {Title, Subtitle, Description, Canvas, Controls} from '@storybook/blocks';
+
 import body from '../../stories/LoremIpsum';
 
 import ConfirmDialog from './ConfirmDialog';
+import Button from './Button';
 
 export default {
 	title: 'Components/ConfirmDialog',
 	component: ConfirmDialog,
+	parameters: {
+		controls: {hideNoControlsWarning: true},
+		docs: {
+			page: () => (
+				<>
+					<Title />
+					<Subtitle />
+					<Description />
+					<Canvas of={TryItOut} />
+					<Controls />
+				</>
+			),
+		},
+	},
 };
 
 export const Default = {
@@ -13,4 +31,21 @@ export const Default = {
 		body,
 		confirmText: 'Confirm',
 	},
+};
+
+const WiredComponent = () => {
+	const [visible, setVisible] = useState(false);
+	const show = useCallback(() => setVisible(true), []);
+	const hide = useCallback(() => setVisible(false), []);
+	return (
+		<>
+			<Button text="Show dialog" type="primary" onClick={show} />
+			{visible && <ConfirmDialog title="Title" body={body} confirmText="Confirm" onClose={hide} onConfirm={hide} />}
+		</>
+	);
+};
+
+export const TryItOut = {
+	name: 'Try it out!',
+	render: () => <WiredComponent />,
 };
--- packages/pelagos/src/components/MultiColumn.stories.js
+++ packages/pelagos/src/components/MultiColumn.stories.js
@@ -1,4 +1,7 @@
+import {useState} from 'react';
+
 import MultiColumn from './MultiColumn';
+import LabelLine from './LabelLine';
 
 const columns = [
 	['a', 'b', 'c'],
@@ -13,6 +16,9 @@ const renderItem = (path) => columns[path.length - 1][path[path.length - 1]];
 export default {
 	title: 'Components/MultiColumn',
 	component: MultiColumn,
+	parameters: {
+		controls: {hideNoControlsWarning: true},
+	},
 };
 
 export const Default = {
@@ -25,4 +31,33 @@ export const Loading = {
 
 export const Empty = {
 	args: {path: [], emptyText: 'No data available'},
+};
+
+const getItemCountWired = () => new Promise((resolve) => setTimeout(() => resolve(26), 500));
+const isLeafWired = (path) => path.length >= 5;
+const renderItemWired = (path) => String.fromCharCode(...path.map((i) => 97 + i));
+
+const WiredComponent = () => {
+	const [path, setPath] = useState([0]);
+	return (
+		<div className="AppMulti">
+			<LabelLine id="label" text="Letters" />
+			<MultiColumn
+				className="AppMulti__multi"
+				path={path}
+				colWidth="16em"
+				emptyText="No letters available"
+				getItemCount={getItemCountWired}
+				isLeaf={isLeafWired}
+				aria-labelledby="label"
+				renderItem={renderItemWired}
+				onChange={setPath}
+			/>
+		</div>
+	);
+};
+
+export const TryItOut = {
+	name: 'Try it out!',
+	render: () => <WiredComponent />,
 };
