d5a5529f717dd1056bcc9fc30177efba9369c616
JC Estibariz <jestibariz@bluecatnetworks.com>
2023-07-13 16:15:33 -0400

    docs: add "Try it out" stories for a few components

--- packages/pelagos/src/components/Dialog.stories.js
+++ packages/pelagos/src/components/Dialog.stories.js
@@ -1,3 +1,6 @@
+import {useCallback, useState} from 'react';
+import {Title, Subtitle, Description, Canvas, Controls} from '@storybook/blocks';
+
 import body from '../../stories/LoremIpsum';
 
 import Dialog from './Dialog';
@@ -6,6 +9,20 @@ import Button from './Button';
 export default {
 	title: 'Components/Dialog',
 	component: Dialog,
+	parameters: {
+		controls: {hideNoControlsWarning: true},
+		docs: {
+			page: () => (
+				<>
+					<Title />
+					<Subtitle />
+					<Description />
+					<Canvas of={TryItOut} />
+					<Controls />
+				</>
+			),
+		},
+	},
 };
 
 export const Default = {
@@ -20,4 +37,30 @@ export const Default = {
 			</div>,
 		],
 	},
+};
+
+const WiredComponent = () => {
+	const [visible, setVisible] = useState(false);
+	const show = useCallback(() => setVisible(true), []);
+	const hide = useCallback(() => setVisible(false), []);
+	return (
+		<>
+			<Button text="Show dialog" type="primary" onClick={show} />
+			{visible && (
+				<Dialog title="Title">
+					<div>
+						<p>{body}</p>
+					</div>
+					<div>
+						<Button text="Close" type="primary" onClick={hide} />
+					</div>
+				</Dialog>
+			)}
+		</>
+	);
+};
+
+export const TryItOut = {
+	name: 'Try it out!',
+	render: () => <WiredComponent />,
 };
