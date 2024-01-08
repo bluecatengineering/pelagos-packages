4955eef79b74e4a60bf01378508c090afc57d27f
JC Estibariz <jestibariz@bluecatnetworks.com>
2024-01-08 14:01:21 -0500

    fix: improve Table styles

--- packages/pelagos/src/icons/index.js
+++ packages/pelagos/src/icons/index.js
@@ -2,4 +2,7 @@ export {default as circleInfoOutline} from './circleInfoOutline';
 export {default as ellipsisThin} from './ellipsisThin';
 export {default as ellipsisVThin} from './ellipsisVThin';
 export {default as rhombusExclamation} from './rhombusExclamation';
+export {default as sort} from './sort';
+export {default as sortDown} from './sortDown';
+export {default as sortUp} from './sortUp';
 export {default as xmarkThin} from './xmarkThin';
