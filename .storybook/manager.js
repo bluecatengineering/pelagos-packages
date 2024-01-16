afa6212cd45f323a4df5db6f761a236da20bbd28
JC Estibariz <jestibariz@bluecatnetworks.com>
2024-01-16 11:53:51 -0500

    docs: set default theme to white

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
 	theme: create({
 		base: 'light',
 		brandTitle: 'Pelagos',
-		appBg: '#f2f4f8',
+		appBg: '#f4f4f4',
 		fontBase: 'Open Sans, sans-serif',
 		textColor: '#161616',
 		textInverseColor: '#ffffff',
