7a8a57084237977a9d7c4f8da319177bae5012b5
Salmon <40319887+chinanwu@users.noreply.github.com>
2024-03-28 06:46:22 -0700

    feat: add action text to Toggle

--- packages/pelagos/__tests__/components/Toggle-test.js
+++ packages/pelagos/__tests__/components/Toggle-test.js
@@ -8,13 +8,21 @@ describe('Toggle', () => {
 	describe('rendering', () => {
 		it('renders expected elements', () => {
 			const wrapper = shallow(
-				<Toggle id="test" className="TestClass" aria-label="Test" checked onChange={jest.fn()} />
+				<Toggle
+					id="test"
+					className="TestClass"
+					aria-label="Test"
+					checked
+					labelOn="foo"
+					labelOff="bar"
+					onChange={jest.fn()}
+				/>
 			);
 			expect(wrapper.getElement()).toMatchSnapshot();
 		});
 
 		it('renders expected elements when className is not set', () => {
-			const wrapper = shallow(<Toggle id="test" aria-label="Test" checked onChange={jest.fn()} />);
+			const wrapper = shallow(<Toggle id="test" aria-label="Test" onChange={jest.fn()} />);
 			expect(wrapper.getElement()).toMatchSnapshot();
 		});
 
@@ -30,7 +38,7 @@ describe('Toggle', () => {
 		it('calls onChange when clicked', () => {
 			const onChange = jest.fn();
 			const wrapper = shallow(<Toggle className="TestClass" aria-label="Test" checked onChange={onChange} />);
-			wrapper.simulate('click');
+			wrapper.find('button').simulate('click');
 			expect(onChange).toHaveBeenCalled();
 		});
 	});
--- packages/pelagos/src/components/Toggle.stories.js
+++ packages/pelagos/src/components/Toggle.stories.js
@@ -1,4 +1,7 @@
+import {useCallback, useState} from 'react';
+
 import Toggle from './Toggle';
+import Button from './Button';
 
 export default {
 	title: 'Components/Toggle',
@@ -15,4 +18,23 @@ export const Checked = {
 
 export const Disabled = {
 	args: {'aria-label': 'Disabled', disabled: true},
+};
+
+const WiredComponent = () => {
+	const [checked, setChecked] = useState(false);
+	const [disabled, setDisabled] = useState(false);
+	const handleChange = useCallback(() => setChecked((o) => !o), []);
+	const handleClick = useCallback(() => setDisabled((o) => !o), []);
+
+	return (
+		<>
+			<Toggle checked={checked} onChange={handleChange} disabled={disabled} />
+			<Button style={{marginTop: '16px'}} text="Disable toggle" type="primary" onClick={handleClick} />
+		</>
+	);
+};
+
+export const TryItOut = {
+	name: 'Try it out!',
+	render: () => <WiredComponent />,
 };
