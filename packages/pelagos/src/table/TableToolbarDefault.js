18af400553b74d11775bbae6365a3e4e1ec917bc
JC Estibariz <jestibariz@bluecatnetworks.com>
2024-05-28 10:41:21 -0400

    fix: add option to align the table toolbar to the start

--- packages/pelagos/src/table/TableToolbarDefault.js
+++ packages/pelagos/src/table/TableToolbarDefault.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 
 import hideChild from './hideChild';
 
+const alignClass = {start: 'toolbarStart', end: 'toolbarEnd'};
+
 /** Table toolbar for default actions. */
-const TableToolbarDefault = forwardRef(({className, hidden, children}, ref) => (
-	<div className={`Table__toolbarDefault${className ? ` ${className}` : ''}`} aria-hidden={hidden} ref={ref}>
+const TableToolbarDefault = forwardRef(({className, hidden, align = 'end', children}, ref) => (
+	<div
+		className={`Table__toolbarDefault Table--${alignClass[align]}${className ? ` ${className}` : ''}`}
+		aria-hidden={hidden}
+		ref={ref}>
 		{hidden ? (Array.isArray(children) ? children.map(hideChild) : hideChild(children, 0)) : children}
 	</div>
 ));
@@ -17,6 +22,8 @@ TableToolbarDefault.propTypes = {
 	className: PropTypes.string,
 	/** Whether this toolbar should be hidden (when TableToolbarBatch is visible). */
 	hidden: PropTypes.bool,
+	/** The alignment for children. */
+	align: PropTypes.oneOf(['start', 'end']),
 	/** The child elements. */
 	children: PropTypes.node,
 };
