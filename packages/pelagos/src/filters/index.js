31d17f964dc8c26a120218dac7118647fadfeb61
JC Estibariz <jestibariz@bluecatnetworks.com>
2024-06-25 11:40:31 -0400

    fix: add missing export for TimeFilterEditor

--- packages/pelagos/src/filters/index.js
+++ packages/pelagos/src/filters/index.js
@@ -6,3 +6,4 @@ export {default as FilterList} from './FilterList';
 export {default as FilterMenu} from './FilterMenu';
 export {default as NameFilterEditor} from './NameFilterEditor';
 export {default as TextFilterEditor} from './TextFilterEditor';
+export {default as TimeFilterEditor} from './TimeFilterEditor';
