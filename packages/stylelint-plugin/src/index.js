41a73e03a374f48f8db3ea3604e253fdaf6e91a4
JC Estibariz <jestibariz@bluecatnetworks.com>
2024-07-05 17:04:59 -0400

    chore: use external @bluecateng/stylelint-config

--- packages/stylelint-plugin/src/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const {createPlugin} = require('stylelint');
-
-module.exports = [
-	createPlugin(`@bluecateng/property-strict-value`, require(`./property-strict-value`)),
-	createPlugin(`@bluecateng/selector-bem`, require(`./selector-bem`)),
-];
