5a5894c211688e3bfe9601afe94c3b8a9339418a
JC Estibariz <jestibariz@bluecatnetworks.com>
2025-06-03 14:43:17 -0400

    refactor: use asynchronous functions in analyze-imports

--- packages/analyze-imports/src/load-imports.js
+++ packages/analyze-imports/src/load-imports.js
@@ -1,59 +1,63 @@
 'use strict';
 
-const {opendirSync, readFileSync} = require('node:fs');
+const {readdir, readFile} = require('node:fs/promises');
 const {basename, join} = require('node:path');
 
 const parser = require('@babel/parser');
 
 const nodeTypes = ['ImportDeclaration', 'ExportNamedDeclaration', 'ExportAllDeclaration'];
 
-const parseJS = (path, shortPath) => {
-	const imports = [];
-	try {
-		const ast = parser.parse(readFileSync(path, 'utf8'), {
-			sourceType: 'module',
-			plugins: ['jsx', 'classProperties', 'doExpressions', 'throwExpressions'],
-		});
-		if (ast.type !== 'File') {
-			console.error(`Unexpected ast type: ${ast.type}`);
+const parseJS = (imports, path, shortPath) =>
+	readFile(path, 'utf8').then(
+		(text) => {
+			try {
+				const ast = parser.parse(text, {
+					sourceType: 'module',
+					plugins: ['jsx', 'classProperties', 'doExpressions', 'throwExpressions'],
+				});
+				if (ast.type !== 'File') {
+					console.error(`Unexpected ast type: ${ast.type}`);
+					process.exit(1);
+				}
+
+				for (const node of ast.program.body) {
+					if (nodeTypes.includes(node.type) && node.source) {
+						const source = node.source.value;
+						if (source[0] === '.') {
+							const sourcePath = join(shortPath, source);
+							imports.set(sourcePath, (imports.get(sourcePath) || 0) + 1);
+						}
+					}
+				}
+			} catch (e) {
+				console.error(`Error parsing ${path}`, e);
+				process.exit(1);
+			}
+		},
+		(error) => {
+			console.error(`Error reading ${path}: ${error.message}`, error);
 			process.exit(1);
 		}
-
-		for (const node of ast.program.body) {
-			if (nodeTypes.includes(node.type) && node.source) {
-				const source = node.source.value;
-				if (source[0] === '.') {
-					imports.push(join(shortPath, source));
-				}
-			}
-		}
-	} catch (e) {
-		console.error(`Error parsing ${path}`, e);
-		process.exit(1);
-	}
-	return imports;
-};
+	);
 
 const scanDir = (map, basePath, shortPath) => {
 	const imports = new Map();
-	const dir = opendirSync(basePath);
-	let entry;
-	while ((entry = dir.readSync())) {
-		const name = entry.name;
-		if (entry.isDirectory()) {
-			scanDir(map, `${basePath}/${name}`, `${shortPath}/${name}`);
-		} else if (entry.isFile() && /\.js$/.test(name) && !/\.stories\.js$/.test(name)) {
-			for (const i of parseJS(`${basePath}/${name}`, shortPath)) {
-				imports.set(i, (imports.get(i) || 0) + 1);
-			}
-		}
-	}
-	dir.closeSync();
 	map.set(shortPath, imports);
+	return readdir(basePath, {withFileTypes: true}).then((entries) =>
+		Promise.all(
+			entries.map((entry) => {
+				const name = entry.name;
+				return entry.isDirectory()
+					? scanDir(map, `${basePath}/${name}`, `${shortPath}/${name}`)
+					: entry.isFile() && /\.js$/.test(name) && !/\.stories\.js$/.test(name)
+						? parseJS(imports, `${basePath}/${name}`, shortPath)
+						: Promise.resolve();
+			})
+		)
+	);
 };
 
 module.exports = (dir) => {
 	const map = new Map();
-	scanDir(map, dir, basename(dir));
-	return map;
+	return scanDir(map, dir, basename(dir)).then(() => map);
 };
