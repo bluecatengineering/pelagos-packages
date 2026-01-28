221fd238a08c0caee17edca94d8b13f669e58b39
JC Estibariz <jestibariz@bluecatnetworks.com>
2026-01-28 13:29:45 -0500

    feat: add @bluecateng/pelagos-highcharts package

---
+++ packages/pelagos-highcharts/src/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Returns a class name appropriate for a subset of colors.
+ * @param className an optional additional class name.
+ * @param count the number of items displayed in the chart.
+ * @param option an optional alternate color set.
+ */
+export const buildColorSetClass: (className?: string, count?: number | null, option?: number) => string | undefined;
