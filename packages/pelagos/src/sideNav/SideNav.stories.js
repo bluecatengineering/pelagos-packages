37d9bc500bd98d8e43c8b6c524c5750eb03c1d39
Jeremy Wertkin <jwertkin@bluecatnetworks.com>
2026-02-03 16:39:50 -0500

    docs: add onClick to menus in SideNav stories

--- packages/pelagos/src/sideNav/SideNav.stories.js
+++ packages/pelagos/src/sideNav/SideNav.stories.js
@@ -1,4 +1,5 @@
 import LogoReact from '@carbon/icons-react/es/LogoReact';
+import {useState, useCallback} from 'react';
 
 import SideNav from './SideNav';
 import SideNavDivider from './SideNavDivider';
@@ -11,94 +12,117 @@ export default {
 	title: 'Components/SideNav',
 	component: SideNav,
 	parameters: {layout: 'fullscreen'},
+	args: {active: true},
+};
+
+const useWiredSideNavMenus = (initialState) => {
+	const [open, setOpen] = useState(initialState);
+
+	const handleClick = useCallback((event) => {
+		const element = event.target.closest('[data-key]');
+		if (element) {
+			const key = element.dataset.key;
+			setOpen((open) => ({...open, [key]: !open[key]}));
+		}
+	}, []);
+
+	return [open, handleClick];
 };
 
 export const Default = {
-	args: {
-		active: true,
-		children: (
-			<SideNavItems style={{width: '256px'}}>
-				<SideNavMenu title="Menu one">
-					<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavMenu title="Menu two" expanded>
-					<SideNavMenuItem current>Menu item two/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavDivider />
-				<SideNavLink href="#">Link one</SideNavLink>
-				<SideNavLink href="#">Link two</SideNavLink>
-			</SideNavItems>
-		),
+	render: (args) => {
+		const [open, handleClick] = useWiredSideNavMenus({one: false, two: true});
+		return (
+			<SideNav {...args} onClick={handleClick}>
+				<SideNavItems style={{width: '256px'}}>
+					<SideNavMenu title="Menu one" expanded={open.one} data-key="one">
+						<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavMenu title="Menu two" expanded={open.two} data-key="two">
+						<SideNavMenuItem current>Menu item two/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavDivider />
+					<SideNavLink href="#">Link one</SideNavLink>
+					<SideNavLink href="#">Link two</SideNavLink>
+				</SideNavItems>
+			</SideNav>
+		);
 	},
 };
 
 export const CollapsedWithCurrent = {
-	args: {
-		active: true,
-		children: (
-			<SideNavItems style={{width: '256px'}}>
-				<SideNavMenu title="Menu one">
-					<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavMenu title="Menu two">
-					<SideNavMenuItem current>Menu item two/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavDivider />
-				<SideNavLink href="#">Link one</SideNavLink>
-				<SideNavLink href="#">Link two</SideNavLink>
-			</SideNavItems>
-		),
+	render: (args) => {
+		const [open, handleClick] = useWiredSideNavMenus({one: false, two: false});
+		return (
+			<SideNav {...args} onClick={handleClick}>
+				<SideNavItems style={{width: '256px'}}>
+					<SideNavMenu title="Menu one" expanded={open.one} data-key="one">
+						<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavMenu title="Menu two" expanded={open.two} data-key="two">
+						<SideNavMenuItem current>Menu item two/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavDivider />
+					<SideNavLink href="#">Link one</SideNavLink>
+					<SideNavLink href="#">Link two</SideNavLink>
+				</SideNavItems>
+			</SideNav>
+		);
 	},
 };
 
 export const CurrentLink = {
-	args: {
-		active: true,
-		children: (
-			<SideNavItems style={{width: '256px'}}>
-				<SideNavMenu title="Menu one">
-					<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavMenu title="Menu two">
-					<SideNavMenuItem>Menu item two/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavDivider />
-				<SideNavLink href="#">Link one</SideNavLink>
-				<SideNavLink href="#" current>
-					Link two
-				</SideNavLink>
-			</SideNavItems>
-		),
+	render: (args) => {
+		const [open, handleClick] = useWiredSideNavMenus({one: false, two: false});
+		return (
+			<SideNav {...args} onClick={handleClick}>
+				<SideNavItems style={{width: '256px'}}>
+					<SideNavMenu title="Menu one" expanded={open.one} data-key="one">
+						<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavMenu title="Menu two" expanded={open.two} data-key="two">
+						<SideNavMenuItem>Menu item two/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavDivider />
+					<SideNavLink href="#">Link one</SideNavLink>
+					<SideNavLink href="#" current>
+						Link two
+					</SideNavLink>
+				</SideNavItems>
+			</SideNav>
+		);
 	},
 };
 
 export const WithIcons = {
-	args: {
-		active: true,
-		children: (
-			<SideNavItems style={{width: '256px'}}>
-				<SideNavMenu title="Menu one" icon={LogoReact}>
-					<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavMenu title="Menu two" icon={LogoReact} expanded>
-					<SideNavMenuItem current>Menu item two/one</SideNavMenuItem>
-					<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
-				</SideNavMenu>
-				<SideNavDivider />
-				<SideNavLink href="#" icon={LogoReact}>
-					Link one
-				</SideNavLink>
-				<SideNavLink href="#" icon={LogoReact}>
-					Link two
-				</SideNavLink>
-			</SideNavItems>
-		),
+	render: (args) => {
+		const [open, handleClick] = useWiredSideNavMenus({one: false, two: true});
+		return (
+			<SideNav {...args} onClick={handleClick}>
+				<SideNavItems style={{width: '256px'}}>
+					<SideNavMenu title="Menu one" icon={LogoReact} expanded={open.one} data-key="one">
+						<SideNavMenuItem>Menu item one/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item one/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavMenu title="Menu two" icon={LogoReact} expanded={open.two} data-key="two">
+						<SideNavMenuItem current>Menu item two/one</SideNavMenuItem>
+						<SideNavMenuItem>Menu item two/two</SideNavMenuItem>
+					</SideNavMenu>
+					<SideNavDivider />
+					<SideNavLink href="#" icon={LogoReact}>
+						Link one
+					</SideNavLink>
+					<SideNavLink href="#" icon={LogoReact}>
+						Link two
+					</SideNavLink>
+				</SideNavItems>
+			</SideNav>
+		);
 	},
 };
