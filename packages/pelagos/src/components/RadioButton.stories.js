13dcff0d6edd9d0ca1ee348e62c1c21e003d36f6
JC Estibariz <jestibariz@bluecatnetworks.com>
2026-02-26 11:13:55 -0500

    fix: handle disabled state in RadioButton

--- packages/pelagos/src/components/RadioButton.stories.js
+++ packages/pelagos/src/components/RadioButton.stories.js
@@ -9,14 +9,18 @@ export const Default = {args: {label: 'Default'}};
 
 export const Checked = {args: {label: 'Checked', checked: true}};
 
+export const Disabled = {args: {label: 'Disabled', disabled: true}};
+
 export const Error = {args: {label: 'Error', error: true}};
 
 export const AllStates = () => (
 	<div className="Story__group">
 		<RadioButton label="Option 1" />
 		<RadioButton label="Option 2" checked />
-		<RadioButton label="Option 3" error />
-		<RadioButton label="Option 4" checked error />
+		<RadioButton label="Option 3" disabled />
+		<RadioButton label="Option 4" checked disabled />
+		<RadioButton label="Option 5" error />
+		<RadioButton label="Option 6" checked error />
 	</div>
 );
 AllStates.storyName = 'All states';
