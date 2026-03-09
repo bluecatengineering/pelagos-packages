d687260f4518bbb6151541da92fa510a74aee5ef
JC Estibariz <jestibariz@bluecatnetworks.com>
2026-03-09 13:51:50 -0400

    fix: limit height of all menus and select pop-ups

--- packages/pelagos/src/menu/ButtonMenu.stories.js
+++ packages/pelagos/src/menu/ButtonMenu.stories.js
@@ -17,8 +17,13 @@ const children = [
 	<MenuItem key="1" text="Option 1" onClick={handleClick} />,
 	<MenuItem key="2" text="Option 2" onClick={handleClick} />,
 	<MenuItem key="3" text="Option 3" disabled onClick={handleClick} />,
+	<MenuItem key="4" text="Option 4" onClick={handleClick} />,
+	<MenuItem key="5" text="Option 5" onClick={handleClick} />,
+	<MenuItem key="6" text="Option 6" onClick={handleClick} />,
+	<MenuItem key="7" text="Option 7" onClick={handleClick} />,
+	<MenuItem key="8" text="Option 8" onClick={handleClick} />,
 	<MenuItemDivider key="d1" />,
-	<MenuItem key="4" text="Danger option" type="danger" onClick={handleClick} />,
+	<MenuItem key="9" text="Danger option" type="danger" onClick={handleClick} />,
 ];
 
 export const Default = {args: {text: 'Actions', children}};
