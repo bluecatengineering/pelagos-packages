352bd71c46cb7fe05d52bf7188952f06a188ad5c
JC Estibariz <jestibariz@bluecatnetworks.com>
2026-08-28 15:27:29 -0400

    fix: adjust horizontal pop-up position if it overflows the screen

--- packages/pelagos/__tests__/hooks/useSelectPositioner-test.js
+++ packages/pelagos/__tests__/hooks/useSelectPositioner-test.js
@@ -9,14 +9,15 @@ const anyFunction = expect.any(Function);
 
 global.document = {addEventListener: jest.fn(), removeEventListener: jest.fn()};
 global.window = {addEventListener: jest.fn(), removeEventListener: jest.fn()};
+global.innerWidth = 600;
 global.innerHeight = 500;
 
 describe('useSelectPositioner', () => {
 	it('sets the pop-up location relative the button', () => {
 		const button = {
-			getBoundingClientRect: () => ({bottom: 100, left: 200, width: 400}),
+			getBoundingClientRect: () => ({bottom: 100, left: 200, right: 600, width: 400}),
 		};
-		const popUp = {style: {}, getBoundingClientRect: () => ({height: 200})};
+		const popUp = {style: {}, getBoundingClientRect: () => ({width: 300, height: 200})};
 		const buttonRef = {current: button};
 		const popUpRef = {current: popUp};
 		const disconnect = jest.fn();
@@ -29,21 +30,21 @@ describe('useSelectPositioner', () => {
 		expect(window.addEventListener.mock.calls).toEqual([['resize', anyFunction, {passive: true, capture: true}]]);
 		expect(addResizeObserver.mock.calls).toEqual([[popUp, anyFunction]]);
 
-		button.getBoundingClientRect = () => ({top: 330, bottom: 350, left: 200, width: 400});
+		button.getBoundingClientRect = () => ({top: 330, bottom: 350, left: 400, right: 800, width: 400});
 		document.addEventListener.mock.calls[0][1]();
-		expect(popUp.style).toEqual({top: '130px', left: '200px', width: '400px'});
+		expect(popUp.style).toEqual({top: '130px', left: '500px', width: '400px'});
 
-		button.getBoundingClientRect = () => ({top: 230, bottom: 250, left: 200, width: 400});
+		button.getBoundingClientRect = () => ({top: 230, bottom: 250, left: 200, right: 600, width: 400});
 		window.addEventListener.mock.calls[0][1]();
 		expect(popUp.style).toEqual({top: '250px', left: '200px', width: '400px'});
 
-		button.getBoundingClientRect = () => ({top: 80, bottom: 100, left: 200, width: 400});
-		popUp.getBoundingClientRect = () => ({height: 480});
+		button.getBoundingClientRect = () => ({top: 80, bottom: 100, left: 100, right: 500, width: 400});
+		popUp.getBoundingClientRect = () => ({width: 580, height: 480});
 		window.addEventListener.mock.calls[0][1]();
-		expect(popUp.style).toEqual({top: '0px', left: '200px', width: '400px'});
+		expect(popUp.style).toEqual({top: '0px', left: '0px', width: '400px'});
 
-		button.getBoundingClientRect = () => ({top: 130, bottom: 150, left: 200, width: 400});
-		popUp.getBoundingClientRect = () => ({height: 100});
+		button.getBoundingClientRect = () => ({top: 130, bottom: 150, left: 200, right: 600, width: 400});
+		popUp.getBoundingClientRect = () => ({width: 300, height: 100});
 		addResizeObserver.mock.calls[0][1]();
 		expect(popUp.style).toEqual({top: '150px', left: '200px', width: '400px'});
 
@@ -55,9 +56,9 @@ describe('useSelectPositioner', () => {
 
 	it('does not set the pop-up width when changePopUpWidth is false', () => {
 		const button = {
-			getBoundingClientRect: () => ({bottom: 100, left: 200, width: 400}),
+			getBoundingClientRect: () => ({bottom: 100, left: 200, right: 600, width: 400}),
 		};
-		const popUp = {style: {width: '100px'}, getBoundingClientRect: () => ({height: 200})};
+		const popUp = {style: {width: '100px'}, getBoundingClientRect: () => ({width: 300, height: 200})};
 		const buttonRef = {current: button};
 		const popUpRef = {current: popUp};
 		useSelectPositioner(true, buttonRef, popUpRef, {changePopUpWidth: false});
